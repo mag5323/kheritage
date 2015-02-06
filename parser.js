10a6f1834063b58a0fb437bbf2360d62b22ad5ab
mag5323 <mag5323@gmail.com>
2015-02-06 20:00:56 +0800

    parse Heritage_desc

--- parser.js
+++ parser.js
@@ -1,3 +1,12 @@
 d3.json('data/heritage.json')
   .get(function(error, data) {
+    var heritages = data;
+
+    for (key in heritages) {
+      var descs = heritages[key].Heritage_desc.split('\n');
+
+      for (key in descs) {
+        var desc = descs[key].split('：');
+      }
+    }
   });
